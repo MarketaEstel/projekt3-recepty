ecd68d5a32d2bf94919678fba901e490afdd8a87
MarketEstel <estelka.m@gmail.com>
2022-04-28 12:06:16 +0200

    aktivován input hledání

--- app.js
+++ app.js
@@ -18,21 +18,36 @@ recept-hodnoceni, recept-nazev, recept-popis.
 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
 
-
-
-function vypisRecepty(){
-    
+function vypisRecepty() {
+  
 }
 
 for (let i=0; i < recepty.length; i++) {
-  const nazevReceptu = document.createElement("div")
-  nazevReceptu.classList.add("recept-info")
-  nazevReceptu.textContent = recepty[i].nadpis
-    
-  const fotoReceptu = document.querySelector(".recept-obrazek")
-  fotoReceptu.src = recepty[i].img
+  const seznamReceptu = document.createElement("div")
+  seznamReceptu.classList.add("recept")
+  
+  let receptFoto = document.createElement("div")
+  receptFoto.classList.add("recept-obrazek")
+
+  let imgRecept = document.createElement("img")
+  imgRecept.src = recepty[i].img
+
+  receptFoto.appendChild(imgRecept)
+
+  const receptInfo = document.createElement("div")
+  receptInfo.classList.add("recept-info")
+  
+  let nadpisRecept = document.createElement("h3")
+  nadpisRecept.textContent = recepty[i].nadpis
+  receptInfo.appendChild(nadpisRecept)
+
+  seznamReceptu.appendChild(receptFoto)
+  seznamReceptu.appendChild(receptInfo)
+
+  const receptyItem = document.getElementById("recepty")  
+  receptyItem.appendChild(seznamReceptu)
 }
-// let receptySeznam = document.getElementById(recepty)
+
 
 // const JedenRecept = (props) => {
 //   const element = document.createElement('div');
@@ -61,3 +76,17 @@ for (let i=0; i < recepty.length; i++) {
 //   return element;
 // };
 
+//const recepty už mám v druhém JS const recepty - je to pole
+const inputFilter = document.querySelector(".hledani") //naše pole pro hledání
+console.log(inputFilter)
+
+const receptySearch = []
+
+//pridatData()
+
+inputFilter.addEventListener("input", function(e){
+  console.log(e.target.value)   
+  receptyFilter(e.target.value) 
+})
+
+//console.log(e.target.value) toto mi vypisuje v konzoli, co píšu do inputu a potřebuji to poslat do funkce
